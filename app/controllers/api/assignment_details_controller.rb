58bb1c4e2de5636c2626e34dfb8174739db63d29
Dan Actor <danltactor@gmail.com>
2018-03-28 15:28:11 -0600

    one more basic update

--- app/controllers/api/assignment_details_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::AssignmentDetailsController < ApplicationController
-  def show
-    `cp #{Rails.root}/README.md #{Rails.root}/public`
-    render json: { file: File.read(Rails.root.join('public', 'README.md')) }
-  end
-end
